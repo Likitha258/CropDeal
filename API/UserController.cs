d1337b3710e6eb0d73a58b80bc71cd97f1e211e8
Likitha258 <71436387+Likitha258@users.noreply.github.com>
2023-01-11 06:52:02 +0000

    API Part
    Uploaded Successfully...!

---
+++ API/UserController.cs
@@ -0,0 +1,34 @@
+﻿using CropAPI.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CropAPI.Models;
+
+
+
+namespace CropAPI.Controllers
+{
+    public class UserController : ApiController
+    {
+        IDataRepository<UserProfile> _dataRepository;
+        public UserController()
+
+
+
+
+
+        {
+            this._dataRepository = new UserRepository(new CDProEntities());
+        }
+        [HttpGet]
+        [Route("")]
+        public IEnumerable<UserProfile> GetAllUsers()
+        {
+            var users = _dataRepository.GetAll();
+            return users;
+        }
+    }
+}
